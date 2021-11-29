8892b6e8dd8e992fe2aa183b315b8a740a772abe
HasabAlrasool <r0000tbug@yahoo.com>
2021-11-29 11:35:13 +0300

    Initial Commit Adding Code

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Tax Invoice First Stage',
+    'version': '1.0',
+    'installed_version': '1.0',
+    'category': 'Accounting',
+    'author': 'Hasabalrasool',
+    'summary': 'Meet the first staget requirement for the Saudi Arabia Tax Invoice and Simple Invoice',
+    'website': 'https://www.linkedin.com/in/r00tbug',
+    'description': """
+    -Add an automaticly generated QRCode in invoice form views and reports with the required information.
+    -Disable invoice delete after the posted State
+    -Disable invoice modification after the posted State
+    -Change the views and reports design to meet the fist stage requirements.
+    """,
+    'depends': [ 'account' ],
+    'data': [
+        'report/account_invoice_report_template.xml',
+        'views/account_move_inherit.xml',
+    ],
+    'images': [ 'static/description/banner.png', ],
+    'installable': True,
+    'application': True,
+    'price': 100,
+    'currency': 'SAR',
+    'license': "LGPL-3",
+}
---
+++ models/account_move.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api ,_
+from odoo.exceptions import UserError
+import qrcode
+import base64
+import io
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class Invoice(models.Model):
+    _inherit = 'account.move'
+
+    qr_code_image = fields.Binary("QRCode Image", compute='_generate_qr_code')
+    company_vat = fields.Char(string='Company / Vendor Vat',related="company_id.vat",store=True)
+
+
+    def _generate_qr_code(self):
+        qr_info = ''
+        required_fields = {'Company':'company_id', 'Company Vat':'company_vat', 'Last Updated on':'write_date', 'Total':'amount_total', 'Tax':'amount_tax'}
+        required_fields_attributes = self.env['ir.model.fields'].search([('model_id.model','=','account.move'), ('name','in',list(required_fields.values()))])
+        data = {}
+        for field_info in required_fields_attributes:
+            if field_info.ttype == 'many2one':
+                if field_info.name == "company_id":
+                    qr_info += _('Company / Vendor Name') + " : " + self[field_info.name].display_name + "\n"
+                    continue
+                else:
+                    qr_info += f"{field_info.field_description} : {self[field_info.name].display_name} \n"
+            else:
+                if field_info.name == "write_date":
+                    qr_info += _('Timestamp') +" : " + self[field_info.name].strftime('%Y-%M-%d %H:%m:%S') + "\n"
+                elif field_info.name == "amount_total":
+                    qr_info += _('Total With Tax') + " : " + str(self[field_info.name]) + "\n"
+                elif field_info.name == "amount_tax":
+                    qr_info += _('Total VAT') + " : " + str(self[field_info.name]) + "\n"
+        _logger.info(qr_info)
+        data = io.BytesIO()
+        qrcode.make(qr_info, box_size=4).save(data, optimise=True, format='PNG')
+        self.qr_code_image = base64.b64encode(data.getvalue()).decode()
+
+    def unlink(self):
+        if self.state != "draft":
+            raise UserError(_("This Record Can't Be deleted"))
+        return super(Invoice, self).unlink()
+
+    def write(self,vals):
+        if vals == {}:
+            return
+        if self.state == "posted":
+            if ('access_token' not in str(vals) and 'invoice_payment_ref' not in str(vals) and "name" not in str(vals) and "message_main_attachment_id" not in str(vals) and "tax_country_id" not in str(vals)):
+                _logger.info("data : ")
+                _logger.info(vals)
+                raise UserError(_("This Record Can't Be Modified"))
+        return super(Invoice, self).write(vals)
